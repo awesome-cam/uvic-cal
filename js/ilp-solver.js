b8aa7b78f22b91bfe8547b204c99cd20698ad9af
Karla Bowman <kbozman1978@gmail.com>
2026-05-17 15:36:52 -0700

    change to a Java solver

--- js/ilp-solver.js
+++ js/ilp-solver.js
@@ -1053,7 +1053,7 @@ async function solveWithILP(
 ) {
 
     const glpkInstance =
-        await GLPK();
+        solver;
 
     const groupModels =
         buildGroupModels(
@@ -1454,7 +1454,7 @@ async function solveSingleForcedSchedule(
 ) {
 
     const glpkInstance =
-        await GLPK();
+        solver;
 
     const groupModels =
         buildGroupModels(
