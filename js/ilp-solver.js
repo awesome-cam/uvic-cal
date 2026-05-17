3bf9fb2e39472b33ada5af3643ce10f4f350faae
Karla Bowman <kbozman1978@gmail.com>
2026-05-17 15:27:41 -0700

    update website

--- js/ilp-solver.js
+++ js/ilp-solver.js
@@ -1053,7 +1053,7 @@ async function solveWithILP(
 ) {
 
     const glpkInstance =
-        await window.loadGlpk();
+        await GLPK();
 
     const groupModels =
         buildGroupModels(
@@ -1454,7 +1454,7 @@ async function solveSingleForcedSchedule(
 ) {
 
     const glpkInstance =
-        await window.loadGlpk();
+        await GLPK();
 
     const groupModels =
         buildGroupModels(
