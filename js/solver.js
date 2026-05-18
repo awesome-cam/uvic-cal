c851f7c4495193bcd70b237733f97b60ab12f2fb
Karla Bowman <kbozman1978@gmail.com>
2026-05-17 17:05:00 -0700

    go live with backend

--- js/solver.js
+++ js/solver.js
@@ -118,7 +118,7 @@ async function generateSchedules(
     const response =
         await fetch(
 
-            'http://127.0.0.1:8000/solve',
+            'https://uvic-cal-backend.onrender.com/solve',
 
             {
 
